fcb3c6a6e14efef86b7c40f25b0d900a06ec1ba1
Koa Kekuna <kekoaponolani@gmail.com>
2018-04-18 20:42:22 -0400

    add gulp, sass, and gitignore files

---
+++ gulp.js
@@ -0,0 +1,13 @@
+const gulp = require(gulp);
+const sass = require(gulp-sass);
+
+gulp.task("sass", () => {
+  return gulp
+    .src("./style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+});
+
+gulp.task("sass:watch", () => {
+  gulp.watch("./style.scss", ["sass"]);
+});
