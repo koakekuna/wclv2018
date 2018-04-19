9857cf428b87290e215a62c8661c33a4069c2d6b
Koa Kekuna <kekoaponolani@gmail.com>
2018-04-18 20:51:12 -0400

    fix gulp imports and sass variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-const gulp = require(gulp);
-const sass = require(gulp-sass);
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task("sass", () => {
   return gulp
     .src("./style.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("./"));
 });
 
 gulp.task("sass:watch", () => {
